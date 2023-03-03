61e65518ca79a39135232512c0931db9ffb1a279
Your Name <you@example.com>
2023-03-03 15:25:56 +0530

    A5

---
+++ B2.tf
@@ -0,0 +1,4 @@
+resource ""[Daws_vpc" ""[Dvpc2"{
+cidr_block=""[D10.0.0.0/24[D[D[D[D[1[C[C[C[C[C"[C
+Tags=""[D{
+}
---
+++ B3.tf
@@ -0,0 +1,5 @@
+resource "aws_vpc" "vpc2"{
+cidr_block="10.0.1.0/24"
+Tags={
+}
+}
